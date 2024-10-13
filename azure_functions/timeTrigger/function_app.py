783c27645f4426becccd10db70d224bd70707cfb
Abdelrashied, Mostafa <Mostafa.Abdelrashied@Bain.com>
2024-10-13 15:28:57 +0200

    trigger it randomly

--- azure_functions/timeTrigger/function_app.py
+++ azure_functions/timeTrigger/function_app.py
@@ -7,7 +7,7 @@ app = func.FunctionApp()
 
 
 @app.timer_trigger(
-    schedule="*/5 * * * * *",
+    schedule="0 0 1 * *",
     arg_name="myTimer",
     run_on_startup=False,
     use_monitor=False,
